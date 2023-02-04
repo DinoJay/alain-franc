c05cbc52908878fdd7ec92879838b93e05472f4d
DinoJay <jott88@googlemail.com>
2023-02-04 11:30:14 +0100

    img loader

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,26 @@
 <script>
+	import { onMount } from 'svelte';
 	import ImgFlip from '../lib/ImgFlip.svelte';
+
+	let pr;
+	onMount(() => {
+		pr = Promise.all(
+			Array.from(document.images)
+				.filter((img) => !img.complete)
+				.map(
+					(img) =>
+						new Promise((resolve) => {
+							img.onload = img.onerror = resolve;
+						})
+				)
+		).then(() => {
+			console.log('images finished loading');
+		});
+	});
 </script>
 
-<ImgFlip />
+{#await pr}
+	<div>...</div>
+{:then _}
+	<ImgFlip />
+{/await}
