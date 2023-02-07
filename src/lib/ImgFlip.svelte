697a6bad79b9fd253c5bbbc891f7af4dff099888
DinoJay <jott88@googlemail.com>
2023-02-07 20:24:37 +0100

    new pic

--- src/lib/ImgFlip.svelte
+++ src/lib/ImgFlip.svelte
@@ -5,19 +5,42 @@
 
 	setTimeout(() => {
 		visible = true;
-	}, 7300);
+	}, 8300);
 </script>
 
 <div
-	class="bg-cover bg h-screen w-screen"
-	style="{`background-image: url(/gif_E.webp); transform: rotate(-0deg) `} "
+	class="bg-contain bg bg-center h-screen w-screen"
+	style="{`background-image: url(/A_low.webp); transform: rotate(-0deg) `} "
 >
 	{#if visible}
 		<div class="w-full h-full bg-white text" transition:fade>
-			Ils font tutu-panpan à l'automne et emménagent au pringtemps suivant. Ils rénovent une maison
-			de vacances et s'échangent leurs promesses. Ensemble ils vont se dire Oui ! Alain + François
-			se marient. Ils ont le plaisir de vous accueillir sous le thème de la folie et de l'élégance
-			tropicale Le 15 juillet 2023 à 16h R. Conselheiro Dr. Antero P. Leitão 13, 3060-082, Portugal
+			<p>Ils font "Tutu-panpan" à l'automne et emménagent ensemble au printemps suivant.</p>
+
+			<p>Fazem "Tchiki boum" no Outono e mudam-se juntos na Primavera seguinte.</p>
+			<p>Ils rénovent une maison de vacances et rêvent d'y célébrer leur union.</p>
+			<p>Eles estão a renovar uma casa de férias e sonham em celebrar a sua união lá.</p>
+			<p>Ensemble ils vont se dire Oui !</p>
+			<p>Juntos eles dirão Sim!</p>
+			<p>Alain + François</p>
+			<p>se marient / casam</p>
+			<p>
+				Ils ont le plaisir de vous accueillir, sous le thème de l'exubérance et de l'élégance
+				tropicale
+			</p>
+			<p>Têm o prazer de vos convidar, sob o tema da exuberância e da elegância tropical.</p>
+			<p>15.07.2023 - 16h00</p>
+			<p>R. Conselheiro Dr. Antero P. Leitão 13, 3060-082, Portugal</p>
+
+			<div>
+				<p>
+					Deux possibilités d'hébergement : - camping sur place. - hôtel et gîtes dans les environs
+					(à définir).
+				</p>
+				<p>
+					Duas possibilidades de alojamento: - acampamento no local. - hotel o alojamento nas
+					proximidades (a definir).
+				</p>
+			</div>
 		</div>
 	{/if}
 </div>
@@ -33,7 +56,7 @@
 		background-repeat: no-repeat;
 		animation-delay: 1s;
 		animation-name: photos;
-		animation-duration: 6s;
+		animation-duration: 8s;
 		/* animation-iteration-count: 1; */
 		/* animation-timing-function: ease-out; */
 	}
@@ -73,26 +96,26 @@
 	} */
 	@keyframes photos {
 		0% {
-			background-image: url(/gif_A.webp);
+			background-image: url(/A_low.webp);
 			transform: scale(1) rotate(0deg);
 		}
 
 		25% {
 			background-image: url(/gif_B.webp);
-			transform: scale(1.2) rotate(25deg);
+			transform: scale(1.5) rotate(-180deg);
 		}
 
 		50% {
 			background-image: url(/gif_C.webp);
-			transform: scale(1) rotate(50deg);
+			transform: scale(1) rotate(180deg);
 		}
 
 		75% {
 			background-image: url(/gif_D.webp);
-			transform: scale(1.2) rotate(-25deg);
+			transform: scale(2) rotate(-25deg);
 		}
 		100% {
-			background-image: url(/gif_E.webp);
+			background-image: url(/gif_A.webp);
 			transform: scale(1) rotate(0deg);
 			/* content: 'babababa'; */
 		}
